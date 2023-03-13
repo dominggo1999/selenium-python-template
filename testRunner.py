b1752bb3265584555784d7791abc882ab73c80ab
r0 <64012087+dominggo1999@users.noreply.github.com>
2023-03-13 22:03:13 +0700

    only use pyvirtualdisplay on github actions

--- testRunner.py
+++ testRunner.py
@@ -1,13 +1,19 @@
 import unittest
 from e2e.login_page.login import SuccessfulLogin
 import chromedriver_autoinstaller
-# from pyvirtualdisplay import Display
+import os
 
-# display = Display(visible=0, size=(800, 800))
-# display.start()
+# Check if running on Github Actions
+is_running_on_github_actions = os.getenv('GITHUB_ACTIONS') == 'true'
+
+# If running on Github Actions, start virtual display
+if is_running_on_github_actions:
+    from pyvirtualdisplay import Display
+    display = Display(visible=0, size=(1200, 1200))
+    display.start()
+
 
 chromedriver_autoinstaller.install()
-
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
